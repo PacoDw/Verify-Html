d969e1d90712e6ff0ca6a0148cdc964b1695dc89
Paco Preciado Mendoza <__pm@outlook.com>
2018-04-12 22:38:15 -0500

    :wrench: Set the config statics files

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const createError   = require('http-errors');
 const express       = require('express');
-const path          = require('path');
 const cookieParser  = require('cookie-parser');
 const logger        = require('morgan');
+const fileUpload    = require('express-fileupload');
 
 //Require all routes-------------------------------------------------------
 const indexRouter   = require('./routes/index');
@@ -12,7 +12,7 @@ const app = express();
 
 // view engine setup-------------------------------------------------------
 app
-    .set('views', path.join(__dirname, 'views'))
+    .set('views',`${__dirname}/views`)
     .set('view engine', 'hbs');
 
 // Middlewares-------------------------------------------------------------
@@ -21,14 +21,15 @@ app
     .use(express.json())
     .use(express.urlencoded({ extended: false }))
     .use(cookieParser())
-    .use(express.static(path.join(__dirname, 'public')));
+    .use('/assets', express.static(`${__dirname}/public`))
+    .use(fileUpload());
 
 // Routes------------------------------------------------------------------
 app
     .use('/', indexRouter)
     .use('/results', resultsRouter);
 
-    
+
 // catch 404 and forward to error handler----------------------------------
 app.use(function(req, res, next) {
   next(createError(404));
